dc45724a94040cb6c325f45cf3cfabc3cba7af02
jazzcodes <jasleenbhambra01@gmail.com>
2024-03-09 14:10:10 -0500

    trying image fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import classNames from 'classnames';
 import Login_board_module from '../src/_codux/boards/login.board.module.scss';
-// import { ReactComponent as BG } from './assets/bg-pattern.png';
-
-import styles from './App.module.scss';
+import * as logo from './assets/logo.png';
+import * as bg from './assets/bg-pattern.png';
 
 function App() {
     return (
         <div className={Login_board_module['login-body']}>
-            <img src="src/assets/bg-pattern.png" alt="" className={Login_board_module['bg-top']} />
+            <img src={bg} alt="" className={Login_board_module['bg-top']} />
             <div className={Login_board_module.login}>
                 <div className={Login_board_module['login-card']}>
                     <div className={Login_board_module['login-card__header']}>
                         <div className={Login_board_module['login-card-logo-container']}>
-                            <img src="/src/assets/logo.png" alt="" />
+                            <img src={logo} alt="" />
                         </div>
                         <h2></h2>Sign in to Uptime
                     </div>
@@ -44,11 +43,7 @@ function App() {
                     <button className={Login_board_module['login-footer-btn']}>Sign Up</button>
                 </div>
             </div>
-            <img
-                src="src/assets/bg-pattern.png"
-                alt=""
-                className={Login_board_module['bg-bottom']}
-            />
+            <img src={bg} alt="" className={Login_board_module['bg-bottom']} />
         </div>
     );
 }
